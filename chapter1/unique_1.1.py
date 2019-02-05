02fa6500b1121e6c52c27db5fb75d0fda7a45356
yavarkhan1997@gmail.com <saim.khan@colorado.edu>
2019-02-05 08:20:33 -0800

    added another function to unique str

--- chapter1/unique_1.1.py
+++ chapter1/unique_1.1.py
@@ -16,7 +16,24 @@ def determine_unique_1(input):
             return "not unique"
     return "unique"
 
-got = determine_unique_1("jaihoo")
+def determine_unique_2(input):
+    """
+    This function takes a string variable, input, 
+    and determines whether it has all unique characters
+    or not.
+    Example:
+    input = "saim"  Return  "unique"
+    input = "yavar" Return  "not unique"
+    Constraint: No data structures alowed.
+    """
+
+    for i in range(len(input)):
+        for j in range(i+1,len(input)):
+            if(input[i]==input[j]):
+                return "not unique"
+    return "unique"        
+    
+got = determine_unique_2("saim")
 print(got)
 
             
---
+++ misc/class_ex.py
@@ -0,0 +1,7 @@
+class person():
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    def print_name(self):
+        print("the name of this person is {}".format(self.name))
+
